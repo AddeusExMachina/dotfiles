e594ff4db64fdee2a6256076c5ae9fd84ee46196
AddeusExMachina <antaddnf@gmail.com>
2024-04-07 22:40:14 +0200

    Delete colors.lua

--- nvim/.config/nvim/lua/addeusexmachina/packer.lua
+++ nvim/.config/nvim/lua/addeusexmachina/packer.lua
@@ -2,12 +2,10 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        -- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
